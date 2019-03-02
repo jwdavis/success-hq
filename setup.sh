8d0d10748062cf956220b1c03a8b41fd470a3602
jwdavis <jeff@jwdavis.me>
2019-03-01 17:59:08 -0800

    narration
    add some statements to let peple know what's going on during setup

--- setup.sh
+++ setup.sh
@@ -15,36 +15,58 @@
 # limitations under the License.
 
 # enable apis
+echo "Enabling necessary services..."
 gcloud services enable translate.googleapis.com
 gcloud services enable language.googleapis.com
 gcloud services enable dataflow.googleapis.com
 gcloud services enable bigquery-json.googleapis.com
 
 # create dataflow bucket
+echo "Creating GCS bucket..."
 gsutil mb gs://$DEVSHELL_PROJECT_ID-df
 
 # create virtual environment
+echo "Creating Virtual Environment..."
 virtualenv  --python=/usr/bin/python ~/success-hq/shq
 source ~/success-hq/shq/bin/activate
 
 # install python stuff
+echo "Updating pip..."
 pip install --upgrade pip==9.0.1
+
+echo "Installing packages in Cloud Shell"
 pip install --upgrade -r req-1.txt
+
+echo "Installing packages into lib (to be vendored into GAE)"
 pip install --upgrade -r req-2.txt -t lib
 
 # fix protobuf problem in cloud shell
+echo "Uninstalling protobuf in Cloud Shell"
 pip uninstall protobuf -y
+
+echo "Reinstalling protobuf in Cloud Shell"
 pip install protobuf -q
 
 # create app engine app
+echo "Creating GAE app..."
 gcloud app create --region=us-central --quiet
+
+echo "Deploying to GAE..."
 gcloud app deploy app_web/app.yaml api_events/app.yaml queue.yaml index.yaml --quiet
 
 # run dataflow jobs
 cd demo_data
+
+echo "Calling setup URL to create datasets and tables..."
 curl https://$DEVSHELL_PROJECT_ID.appspot.com/setup?what=setup_bq
+
+echo "Running Dataflow pipeline to generate sample data..."
 python datastore_pipeline.py
+
+echo "Running Dataflow pipeline to do sentiment analysis"
 python sentiment.py
 
 # remove unneeded bq table
+
+echo "Removing unneeded BQ table"
 bq rm -f -t events.user_events_load
